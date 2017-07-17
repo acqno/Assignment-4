110f1cf4ad7e710aa130fb2647714c1703602979
alvin <alvin@DESKTOP-391GE8T>
2017-07-17 17:53:32 -0400

    Created the TerrestrialPlanet subclass

---
+++ Assignment 4/TerrestrialPlanet.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+/* 
+ * Name: Alvin Quijano
+ * Date: 07-17-2017
+ * Description: This is the TerrestrialPlanet subclass to the Planet superclass
+ * Version: 0.1 - Created the TerrestrialPlanet subclass
+ */
+
+namespace Assignment_4
+{
+    public class TerrestrialPlanet : Planet
+    {
+        // PRIVATE INSTANCE VARIABLES 
+        private bool _oxygen;
+
+        // CONSTRUCTOR METHOD 
+        public TerrestrialPlanet(string name, double diameter, double mass, bool oxygen)
+            :base(name, diameter, mass)
+        {
+            this._oxygen = oxygen;
+        }
+    }
+}
