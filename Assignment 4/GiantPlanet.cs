21f9fc47398db3bd43355de7fdc0244852fdfbc2
alvin <alvin@DESKTOP-391GE8T>
2017-07-17 17:38:12 -0400

    Added the GiantPlanet subclass

---
+++ Assignment 4/GiantPlanet.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+/* 
+ * Name: Alvin Quijano
+ * Date: 07-17-2017
+ * Description: This is the GiantPlanet subclass to the Planet superclass. 
+ * Version: 0.1 - Created the GiantPlanet subclass
+ */
+
+namespace Assignment_4
+{
+    public class GiantPlanet : Planet
+    {
+        // PRIVATE INSTANCE VARIABLES 
+        private string _type;
+
+        //CONSTRUCTOR METHOD 
+        public GiantPlanet(string name,double diameter, double mass, string type)
+            :base(name,diameter, mass)
+        {
+            this._type = type;
+        }
+
+    }
+}
--- Assignment 4/Planet.cs
+++ Assignment 4/Planet.cs
@@ -7,7 +7,7 @@ using System.Text;
  * Name: Alvin Quijano
  * Date: 07-17-2017
  * Description: This is the abstract class planet
- * Version: 0.1 - Created the abstract class Planet with the private instance variable and public properties
+ * Version: 0.2 - Created the constructor method and the public override ToString method
  */
 
 namespace Assignment_4
@@ -30,9 +30,21 @@ namespace Assignment_4
         public string Name { get; }
         public double OrbitalPeriod { get; set; }
         public int RingCount { get; set; }
-        public double RotationPeriod { get; set; }\
+        public double RotationPeriod { get; set; }
 
+        // CONSTRUCTOR METHOD 
+        public Planet(string name, double diameter, double mass)
+        {
+            this._name = name;
+            this._diameter = diameter;
+            this._mass = mass;
+        }
 
+        // PUBLIC METHODS 
+        public override string ToString()
+        {
+            return string.Format("Name: {0}  Diameter: {1}  Mass: {2}", _name, _diameter, _mass);
+        }
 
     }
 }
