eea6afda3ac06afbbe9ca052d16db62f2263dcb4
alvin <alvin@DESKTOP-391GE8T>
2017-07-20 16:13:50 -0400

    Created and implemented IHabitable intetrface into TerrestrialPlanet class

---
+++ Assignment 4/IHabitable.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+/* 
+ * Name: Alvin Quijano
+ * Date: 07-20-2017
+ * Description: This is the IHabitable interface
+ * Version: 0.1 - Created the IHabitable interface that will be implemented into the TerrestrialPlanet subclass
+ */
+
+namespace Assignment_4
+{
+    public interface IHabitable
+    {
+        bool Habitable();
+    }
+}
--- Assignment 4/TerrestrialPlanet.cs
+++ Assignment 4/TerrestrialPlanet.cs
@@ -5,14 +5,14 @@ using System.Text;
 
 /* 
  * Name: Alvin Quijano
- * Date: 07-17-2017
+ * Date: 07-20-2017
  * Description: This is the TerrestrialPlanet subclass to the Planet superclass
- * Version: 0.1 - Created the TerrestrialPlanet subclass
+ * Version: 0.2 - Implemented the IHasMoons and IHabitable interfaces. 
  */
 
 namespace Assignment_4
 {
-    public class TerrestrialPlanet : Planet
+    public class TerrestrialPlanet : Planet, IHasMoons, IHabitable
     {
         // PRIVATE INSTANCE VARIABLES 
         private bool _oxygen;
@@ -23,5 +23,40 @@ namespace Assignment_4
         {
             this._oxygen = oxygen;
         }
+
+        // PUBLIC METHODS 
+
+        /// <summary>
+        /// This is the HasMoons public bool method
+        /// </summary>
+        /// <returns> True - if MoonCount is greater than 0, otherwise false </returns>
+        public bool HasMoons()
+        {
+            if (MoonCount > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// This is the Habitable public bool method 
+        /// </summary>
+        /// <returns> True - if oxygen bool variable is set to true, otherwise false</returns>
+        public bool Habitable()
+        {
+            if (_oxygen == true)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
     }
 }
